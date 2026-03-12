e4208479348c40c506638d220755270e3d24d8a8
Aishwarya Nishad <aishwaryanishad02@gmail.com>
2026-03-12 20:23:00 +0530

    implement authentication routes, add User model, and configure MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import dotenv from "dotenv"
-
+import authRoutes from "./routes/authroutes.js"
+import connectToMongoDB from "./db/connectMongoDB.js";
+dotenv.config();
 const app = express();
 
-dotenv.config();
+
 
 const PORT  =process.env.PORT ||5000;
 
@@ -11,5 +13,8 @@ app.get("/",(req,res)=>{
      res.send("Hello world")
 })
 
-app.listen(PORT,()=>
-    console.log(`server is running on port ${PORT}`));
+app.use("/api/auth",authRoutes)
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+  console.log(`server is running on port ${PORT}`)});
