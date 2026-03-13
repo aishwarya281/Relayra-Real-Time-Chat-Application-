acf9354a41e3c851ff647953981aeafd7b7a5960
Aishwarya Nishad <aishwaryanishad02@gmail.com>
2026-03-13 11:19:28 +0530

    Implement SignUp, Login, Logout endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,20 @@ import express from "express";
 import dotenv from "dotenv"
 import authRoutes from "./routes/authroutes.js"
 import connectToMongoDB from "./db/connectMongoDB.js";
-dotenv.config();
+
+
+
 const app = express();
-
-
-
 const PORT  =process.env.PORT ||5000;
 
-app.get("/",(req,res)=>{
-     res.send("Hello world")
-})
+dotenv.config();
+
+app.use(express.json());  // to parse the incoming requests with json payloads(from req.body).
+
 
 app.use("/api/auth",authRoutes)
+
+
 
 app.listen(PORT,()=>{
     connectToMongoDB();
