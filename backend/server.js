d61bd2e518df81cdf4a4093c36f2558465173cb8
Aishwarya Nishad <aishwaryanishad02@gmail.com>
2026-03-13 16:28:10 +0530

    Implemented chat messaging backend with Message and Conversation models, protected routes, and APIs for sending and retrieving messages

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
 import express from "express";
 import dotenv from "dotenv"
-import authRoutes from "./routes/authroutes.js"
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
 import connectToMongoDB from "./db/connectMongoDB.js";
+import userRoutes from "./routes/user.routes.js"
+import cookieParser from "cookie-parser";
+
+dotenv.config();
 
 
 
 const app = express();
 const PORT  =process.env.PORT ||5000;
 
-dotenv.config();
 
-app.use(express.json());  // to parse the incoming requests with json payloads(from req.body).
+ app.use(express.json());  // to parse the incoming requests with json payloads(from req.body).
+app.use(cookieParser());
 
+app.use("/api/auth",authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.use("/api/auth",authRoutes)
 
 
 
